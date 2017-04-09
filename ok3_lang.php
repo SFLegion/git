f23111ea764a0713aa5d9ab38f95198007fad504
SFLegion <smeets.fabien@gmail.com>
2017-04-09 23:28:11 +0200

    Add files via upload

--- ok3_lang.php
+++ ok3_lang.php
@@ -1,5 +1,5 @@
 <?php 
 $myfile = fopen("/challenge/web-serveur/ch13/index.php", "r") or die("Unable to open file!");
-echo fgets($myfile);
+echo fread($myfile,filesize("/challenge/web-serveur/ch13/index.php"));
 fclose($myfile);
 ?>
