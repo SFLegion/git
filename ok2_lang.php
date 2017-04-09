3e2f8d97590ffa674fdd8443db5c088cc68d49f4
SFLegion <smeets.fabien@gmail.com>
2017-04-09 23:21:58 +0200

    Add files via upload

--- ok2_lang.php
+++ ok2_lang.php
@@ -1,18 +1,5 @@
-<?php echo getcwd() . "\n";
-
-/*
-$dir = '/challenge/web-serveur/ch13/';
-$files1 = scandir($dir);
-
-print_r($files1);
-
-echo "<br><br><br>";*/
-
-
-
+<?php 
 $myfile = fopen("/challenge/web-serveur/ch13/index.php", "r") or die("Unable to open file!");
 echo fgets($myfile);
 fclose($myfile);
-
-
 ?>
